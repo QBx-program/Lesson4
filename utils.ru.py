85d8b7a3795b694cd724d52c991e66e3fa942160
QBx <ortynskiy-k.ros@mail.ru>
2021-05-23 16:11:12 +0300

    Установлен модуль requests и написан код проверки запуска программы

--- utils.ru.py
+++ utils.ru.py
@@ -1,11 +1,12 @@
-import cur_rate
-
-get_cur = (input('Введите банковское наименование валюты ')).upper()
-ans_cur = cur_rate.currency_rates(get_cur)
-if ans_cur.get(get_cur):
-    print(f'{get_cur}/RUB = {ans_cur[get_cur][1]} за {ans_cur[get_cur][0]} рубль(-ей) на {ans_cur[get_cur][2]}')
+def file_run():
+    import cur_rate
+    get_cur = (input('Введите банковское наименование валюты ')).upper()
+    ans_cur = cur_rate.currency_rates(get_cur)
+    if ans_cur.get(get_cur):
+        print(f'{get_cur}/RUB = {ans_cur[get_cur][1]} за {ans_cur[get_cur][0]} рубль(-ей) на {ans_cur[get_cur][2]}')
 
 def main(argv):
+    import cur_rate
     if len(argv) > 0:
         print('Ответ из консоли:')
         ans_cur = cur_rate.currency_rates(str(argv[1]))
@@ -19,3 +20,5 @@ if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         exit(main(sys.argv))
+    else:
+        file_run()
