92e340c4aa138caba2c6a20c9964c2d53c3e9dc5
QBx <ortynskiy-k.ros@mail.ru>
2021-05-23 16:41:47 +0300

    Исправил описание конвертации

--- utils.ru.py
+++ utils.ru.py
@@ -3,7 +3,7 @@ def file_run():
     get_cur = (input('Введите банковское наименование валюты ')).upper()
     ans_cur = cur_rate.currency_rates(get_cur)
     if ans_cur.get(get_cur):
-        print(f'{get_cur}/RUB = {ans_cur[get_cur][1]} за {ans_cur[get_cur][0]} рубль(-ей) на {ans_cur[get_cur][2]}')
+        print(f'{get_cur}/RUB = {ans_cur[get_cur][1]} рублей за {ans_cur[get_cur][0]} {get_cur} на {ans_cur[get_cur][2]}')
 
 def main(argv):
     import cur_rate
