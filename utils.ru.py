191c1d3675e64926b6ebc95292c5c58b31f347d5
QBx <ortynskiy-k.ros@mail.ru>
2021-05-24 05:11:07 +0300

    Небольшие доработки в utils.ru.py

--- utils.ru.py
+++ utils.ru.py
@@ -1,19 +1,20 @@
+import cur_rate
+
+def answer_rate(a_cur, g_cur):
+    if a_cur.get(g_cur):
+        print(f'{g_cur}/RUB = {a_cur[g_cur][1]} рублей за {a_cur[g_cur][0]} {g_cur} на {a_cur[g_cur][2]}')
+    else:
+        print('Такой валюты не существует')
+
 def file_run():
-    import cur_rate
     get_cur = (input('Введите банковское наименование валюты ')).upper()
     ans_cur = cur_rate.currency_rates(get_cur)
-    if ans_cur.get(get_cur):
-        print(f'{get_cur}/RUB = {ans_cur[get_cur][1]} рублей за {ans_cur[get_cur][0]} {get_cur} на {ans_cur[get_cur][2]}')
+    answer_rate(ans_cur, get_cur)
 
 def main(argv):
-    import cur_rate
-    if len(argv) > 0:
-        ans_cur = cur_rate.currency_rates(str(argv[1]))
-        cur = argv[1].upper()
-        if ans_cur.get(cur):
-            print(f'RUB/{cur} = {ans_cur[cur][1]} за {ans_cur[cur][0]} рубль(-ей) на {ans_cur[cur][2]}')
-        else:
-            print('Такой валюты не существует')
+    ans_cur = cur_rate.currency_rates(str(argv[1]))
+    cur = argv[1].upper()
+    answer_rate(ans_cur, cur)
 
 if __name__ == '__main__':
     import sys
